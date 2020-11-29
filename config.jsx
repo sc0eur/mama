0f86b8ac3288aec6e8c2cc8a7cc31c6e112f4d56
sc0eur <44677804+sc0eur@users.noreply.github.com>
2020-11-29 20:31:24 +0300

    Delete config.jsx

--- config.jsx
+++
@@ -1,4 +0,0 @@
-export const config = {
-    CLIENT_ID: '838797749408-1v9vu8j5h8d5ejc8i8rcp09jit6dj0i7.apps.googleusercontent.com', // Must be taken from Google API Console (https://console.developers.google.com/)
-    API_KEY: 'AIzaSyCqCgpfNcGDVmFhOS_hBfL14ZPoJDnl430' // https://developers.google.com/maps/documentation/javascript/get-api-key
-}
